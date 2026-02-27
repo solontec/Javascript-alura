213ee3629ef6d989a26532b10b890dd77daf4449
gui <guisolon.tec@gmail.com>
2026-02-27 19:42:22 -0300

    Salvando alterações antes do pull

--- init.js
+++ init.js
@@ -1,2 +1,9 @@
-console.log("Olá, teste")
+let frutas = ["maça", "banana"]
 
+frutas.push("Laranja");
+frutas.push("edu")
+
+
+for(let i =0; i < frutas.length ; i++){
+    console.log(frutas[i])
+}
---
+++ testFunction.js
@@ -0,0 +1,14 @@
+function somarAte(n){
+    let soma = 0;
+
+    for(let i = 1; i <= n; i++){
+        soma = soma + i
+    }
+return soma
+}
+
+console.log(somarAte(15))
+
+
+
+
