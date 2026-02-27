6c773ab944a4679e76753b5600833c76d0d4f099
Gui <guisolon.tec@gmail.com>
2026-02-27 08:29:25 -0300

    Reading lines

--- init.js
+++ init.js
@@ -1,2 +1,20 @@
-console.log("Olá, teste")
+const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Digite seu nome: ", (nome) => {
+    rl.question("Digite sua idade: ", (idade) => {
+        console.log('Olá, ${nome}!');
+
+        if (idade >= 18) {
+            console.log("Maior de idade");
+        } else {
+            console.log("Menor de idade");
+        }
+
+        rl.close();
+    });
+});
